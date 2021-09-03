ec4c068e58d651069327e9eee540cb90989036e3
Jan Rehanek <rehanekjan@gmail.com>
2021-09-03 20:20:16 +0200

    Pre-context

---
+++ src/components/subset/subset_navbar/navcard/Navcard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Navcard.module.css'
+
+const Navcard = () => {
+    return ( 
+        <div className={styles.navcard}>
+            Navcard
+        </div>
+     );
+}
+ 
+export default Navcard;
