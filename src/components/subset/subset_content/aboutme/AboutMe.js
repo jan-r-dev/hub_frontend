1504cd68202e0ee1bd1d56072d769ec709608c8d
Jan Rehanek <rehanekjan@gmail.com>
2021-10-19 19:44:18 +0200

    minor work on future article component

--- src/components/subset/subset_content/aboutme/AboutMe.js
+++ src/components/subset/subset_content/aboutme/AboutMe.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import styles from './AboutMe.module.css'
+import ReactEmbedGist from 'react-embed-gist';
 
 const AboutMe = () => {
     return ( 
         <div className={styles.aboutme}>
             AboutMe is green
+            <div>
+                <h1> Test gist </h1>
+                <ReactEmbedGist gist="jan-r-dev/ffcfb73d84dc9380fabf628a598cf116"/>
+                <script type="text/jsx" src="https://gist.github.com/jan-r-dev/ffcfb73d84dc9380fabf628a598cf116.js"></script>
+            </div>
         </div>
      );
 }
