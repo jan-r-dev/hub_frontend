33ad7612e0f97c689f30de3530cdabe44f1e0cbe
Jan Rehanek <rehanekjan@gmail.com>
2021-11-01 18:46:06 +0100

    Notes for future progress added

--- src/components/subset/subset_content/article/Article.js
+++ src/components/subset/subset_content/article/Article.js
@@ -3,6 +3,7 @@ import styles from './Article.module.css';
 
 const Article = (props) => {
 
+    // Process text list from API in accordance with the diagram
     function processData(data) {
         data.text.forEach(el => {
             console.log(el);
@@ -11,6 +12,7 @@ const Article = (props) => {
         return processedData;
     };
 
+    // Fetch data from the Article endpoint with the article_id provided by the prop
     async function fetchData() {
         try {
             const data = await axios.get(`http://localhost:8080/articles/${props.article_id}`);
@@ -22,6 +24,7 @@ const Article = (props) => {
         };
     };
     
+    // Return article
     return ( 
         <div className={styles.Article}>
             {props.content}
@@ -30,3 +33,14 @@ const Article = (props) => {
 };
  
 export default Article;
+
+/* Notes on continuation
+
+1) Modify Portfolio for NavLinks instead of Link for styling
+2) Register new dynamic route in /portfolio for portfolio/:articleLink
+3) Extract route parameter :articleLink in the Article component using useParams
+4) Modify the article endpoint in backend to accept :articleLink instead of the Mongo ObjectID
+5) Add Links to the portfolio cards
+6) Verify that Portfolio routing works and is exact
+
+*/
