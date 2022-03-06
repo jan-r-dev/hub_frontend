4b6ccee7a8a05dfd5c6df82432aee0201f3c403c
jan <jan@localhost.localdomain>
2022-03-06 14:56:37 +0100

    work on article responsiveness

--- src/components/subset/subset_content/article/Article.js
+++ src/components/subset/subset_content/article/Article.js
@@ -17,11 +17,11 @@ const Article = () => {
     async function fetchData() {
         try {
             const data = await axios.get(`http://localhost:8080/articles/${params.articleId}`);
-            
+
             processData(data);
-            
+
         } catch (err) {
-            setArticle(<NotFound details='This article does not exist. Use the navbar to navigate to available content.'/>
+            setArticle(<NotFound details='This article does not exist. Use the navbar to navigate to available content.' />
             )
         };
     };
@@ -42,7 +42,7 @@ const Article = () => {
                     titleClass={styles.articleGistHeading}
                     gist={data.data.snippets[articleIndex]} />
                 );
-            } else if (el.startsWith('@@link-')){
+            } else if (el.startsWith('@@link-')) {
                 processedArticle.push(<a className={styles.articleLink} href={data.data.sources[articleIndex]}>{data.data.sources[articleIndex]}</a>);
             } else {
                 processedArticle.push(<p className={styles.articleText}>{data.data.text[index]}</p>);
@@ -57,15 +57,12 @@ const Article = () => {
         fetchData();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [params.articleId])
-    
-    return ( 
-        <div className={styles.Article}>
 
-            <div className={styles.articleContent}>
-                {article === undefined ? <Loader /> : article}
-            </div>
+    return (
+        <div className={styles.Article}>
+            {article === undefined ? <Loader /> : article}
         </div>
-     );
+    );
 };
- 
+
 export default Article;
